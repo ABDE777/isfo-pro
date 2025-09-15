6696ef7144b19bf146e5807fb522256187d8a063
ABDEL MONIM MAZGOURA <135391027+ABDE777@users.noreply.github.com>
2025-09-16 00:12:29 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Analytics } from "@vercel/analytics/react"; // Add this import
 
-createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <Analytics /> {/* Add this line */}
-  </>
-);
+createRoot(document.getElementById("root")!).render(<App />);
